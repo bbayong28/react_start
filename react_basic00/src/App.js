9671185061f5b8b129216f4f66f1d989375eec12
bbayong28 <ghkdud5795@hanmail.net>
2022-08-25 17:38:26 +0900

    20220825

--- react_basic00/src/App.js
+++ react_basic00/src/App.js
@@ -1,9 +1,14 @@
 import Tab from "./pages/Tab01";
+import MainVisual from "./pages/MainVisual";
+import './basic.css'
+import SyncSlick from './pages/SyncSlick'
 
 const App = () => {
     return (
         <>
-            <Tab />
+            <MainVisual />
+            <hr />
+            <SyncSlick />
         </>
     )
 }
